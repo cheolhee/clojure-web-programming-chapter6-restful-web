3c9d5dc0f1a846760d0edadab674e71358fda0d8
ch <net314@naver.com>
2016-03-16 22:54:34 +0900

    이미지 띄우기

--- swagger-service/src/cljs/swagger_service/core.cljs
+++ swagger-service/src/cljs/swagger_service/core.cljs
@@ -1,104 +1,19 @@
 (ns swagger-service.core
-  (:require [reagent.core :as r]
-            [reagent.session :as session]
-            [secretary.core :as secretary :include-macros true]
-            [goog.events :as events]
-            [goog.history.EventType :as HistoryEventType]
-            [markdown.core :refer [md->html]]
-            [swagger-service.ajax :refer [load-interceptors!]]
-            [ajax.core :refer [GET POST]])
-  (:import goog.History))
-
-(defn nav-link [uri title page collapsed?]
-  [:li.nav-item
-   {:class (when (= page (session/get :page)) "active")}
-   [:a.nav-link
-    {:href uri
-     :on-click #(reset! collapsed? true)} title]])
-
-(defn navbar []
-  (let [collapsed? (r/atom true)]
-    (fn []
-      [:nav.navbar.navbar-light.bg-faded
-       [:button.navbar-toggler.hidden-sm-up
-        {:on-click #(swap! collapsed? not)} "☰"]
-       [:div.collapse.navbar-toggleable-xs
-        (when-not @collapsed? {:class "in"})
-        [:a.navbar-brand {:href "#/"} "swagger-service"]
-        [:ul.nav.navbar-nav
-         [nav-link "#/" "Home" :home collapsed?]
-         [nav-link "#/about" "About" :about collapsed?]]]])))
-
-(defn about-page []
-  [:div.container
-   [:div.row
-    [:div.col-md-12
-     "this is the story of swagger-service... work in progress"]]])
+  (:require [reagent.core :as r :refer [atom]]
+            [ajax.core :refer [GET]])
+  (:require-macros [secretary.core :refer [defroute]]))
 
 
-(defn home-page []
-  [:div
-   [:h2 "Welcome to ClojureScript"]
-    [:p "live code reloading is fun!"]
-    ; [:hz]
-    ]
-
-    )
-
-; (defn home-page []
-;   [:div.container
-;    [:div.jumbotron
-;     [:h1 "Welcome to swagger-service"]
-;     [:p "Time to start building your site!"]
-;     [:p [:a.btn.btn-primary.btn-lg {:href "http://luminusweb.net"} "Learn more »"]]]
-;    [:div.row
-;     [:div.col-md-12
-;      [:h2 "Welcome to ClojureScript"]]]
-;    (when-let [docs (session/get :docs)]
-;      [:div.row
-;       [:div.col-md-12
-;        [:div {:dangerouslySetInnerHTML
-;               {:__html (md->html docs)}}]]])])
-
-(def pages
-  {:home #'home-page
-   :about #'about-page})
-
-(defn page []
-  [(pages (session/get :page))])
-
-;; -------------------------
-;; Routes
-(secretary/set-config! :prefix "#")
-
-(secretary/defroute "/" []
-  (session/put! :page :home))
-
-(secretary/defroute "/about" []
-  (session/put! :page :about))
-
-;; -------------------------
-;; History
-;; must be called after routes have been defined
-(defn hook-browser-navigation! []
-  (doto (History.)
-        (events/listen
-          HistoryEventType/NAVIGATE
-          (fn [event]
-              (secretary/dispatch! (.-token event))))
-        (.setEnabled true)))
-
-;; -------------------------
-;; Initialize app
-(defn fetch-docs! []
-  (GET (str js/context "/docs") {:handler #(session/put! :docs %)}))
+(defn home-page [] (let [links
+(atom ["http://25.media.tumblr.com/Jjkybd3nSafemf3rYocB7QcC_500.jpg"
+"http://25.media.tumblr.com/tumblr_ln4zdhp4Uj1qcnzavo1_500.gif" "http://24.media.tumblr.com/tumblr_m2kmg2VK2a1qhwmnpo1_1280.jpg" "http://24.media.tumblr.com/tumblr_m30w1mNl1w1qgjltdo1_1280.jpg" "http://25.media.tumblr.com/tumblr_m3gm5oqm9e1r73wdao1_500.jpg"])]
+(fn [] [:div
+(for [link @links]
+[:img {:src link}])])))
 
 (defn mount-components []
-  (r/render [#'navbar] (.getElementById js/document "navbar"))
-  (r/render [#'page] (.getElementById js/document "app")))
+  (r/render-component [home-page] (.getElementById js/document "app")))
+
 
 (defn init! []
-  (load-interceptors!)
-  (fetch-docs!)
-  (hook-browser-navigation!)
   (mount-components))
