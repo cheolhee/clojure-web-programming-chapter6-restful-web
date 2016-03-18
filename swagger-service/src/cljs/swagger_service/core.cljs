589131097ff55d77781cc1098353130680f0576d
ch <net314@naver.com>
2016-03-18 22:54:21 +0900

    작동을 안 한다..?

--- swagger-service/src/cljs/swagger_service/core.cljs
+++ swagger-service/src/cljs/swagger_service/core.cljs
@@ -3,18 +3,81 @@
             [ajax.core :refer [GET]])
   (:require-macros [secretary.core :refer [defroute]]))
 
-(defn home-page [] (let [links
-  (atom
-    ["http://25.media.tumblr.com/Jjkybd3nSafemf3rYocB7QcC_500.jpg"
-    "http://25.media.tumblr.com/tumblr_ln4zdhp4Uj1qcnzavo1_500.gif"
-    "http://24.media.tumblr.com/tumblr_m2kmg2VK2a1qhwmnpo1_1280.jpg"
-    "http://24.media.tumblr.com/tumblr_m30w1mNl1w1qgjltdo1_1280.jpg"
-    "http://25.media.tumblr.com/tumblr_m3gm5oqm9e1r73wdao1_500.jpg"])]
-    (fn []
-      [:div
-      (for [link @links]
-        [:img {:src link}])]
-        )))
+
+(defn fetch-links! [links link-count]
+  (GET "/api/cat-links"
+    {:params {:link-count link-count}
+    :handler #(reset! links (vec (partition-all 6 %)))
+    })
+  )
+
+(defn images [links]
+[:div.text-center
+  (for [row (partition-all 3 links)]
+    ^{:key row}
+    [:div.row
+      (for [link row]
+        ^{:key link}
+        [:div.col-sm-4 [:img {:width 400 :src link}]]
+        )
+      ]
+    )
+  ]
+  )
+
+(defn forward [i pages]
+  (if (< i (dec pages)) (inc i) i))
+
+(defn back [i]
+  (if (pos? i) (dec i) i))
+
+(defn nav-link [page i]
+  [:li {:on-click #(reset! page i)
+    :class (when (= i @page) "active")}
+    [:span i]
+    ]
+  )
+(defn pager [pages page]
+  [:div.row.text-center
+    [:div.col-sm-12
+     [:ul.pagination.pagination-lg
+      (concat
+        [[:li
+           {:on-click #(swap! page back pages)
+             :class (when (= @page 0) "disabled")}
+             [:span "<<"]
+             ]
+           ]
+
+           (map (partial nav-link page) (range pages))
+           [[:li
+            {:on-click #(swap! page forward pages)
+              :class (when (= @page (dec pages)) "disabled")
+            }
+            [:span ">>"]
+           ]]
+        )
+      ]
+     ]
+    ]
+  )
+
+(defn home-page []
+  (let [page (atom 0) links (atom nil)]
+ (fetch-links! links 50)
+
+ (fn []
+ [:div
+  [:hr]
+   (if (not-empty @links)
+   [:div [pager (count @links) page]
+    [images (@links @page)]]
+    [:div "Standby for cats!"])
+  [:hr]
+  ]
+  )
+    ))
+
 
 (defn mount-components []
   (r/render-component [home-page] (.getElementById js/document "app")))
